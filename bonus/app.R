9ff2d8af4e8a4a3593e5917297d06db5cc5b3519
Andrzej Romaniuk <49647768+AndrzejRomaniuk@users.noreply.github.com>
2022-06-19 13:50:12 +0200

    Add files via upload

---
+++ bonus/app.R
@@ -0,0 +1,243 @@
+#Wczytywnanie wymaganych bibliotek
+library(ggplot2)
+library(shiny)
+library(DT)
+library(shinythemes)
+
+#Interfejs Użytkownika (standardowy układ z paskiem bocznym)
+ui <- fluidPage(
+  theme = shinytheme("yeti"),          
+  
+  h1("Basic dataset exploration",      
+     align = "center"),                 
+  
+  sidebarLayout(                       
+    
+    sidebarPanel(                      
+      h3("Iput Data"),     
+      fileInput(                        
+        "Dataset",                     
+        "Input dataset",                
+        accept = c(
+          "text/csv",
+          "text/comma-separated-values,text/plain",
+          ".csv"
+        )
+      ),
+      textInput(                        
+        "ColumnFactor",                 
+        "Factor column name",
+        value = NULL
+      ),
+      textInput(                        
+        "ColumnData",
+        "Data column name",
+        value = NULL
+      ),
+      checkboxInput(                    
+        inputId = "Joint",                
+        label = "Factors Joint?",           
+        value = TRUE
+      ),                       
+      selectInput(                       
+        inputId = "BinsHow",          
+        label = h4("Bin Scale"),         
+        choices = list(
+                    "Small (Scale)"  = 0,
+                    "Large (Input)" = 1 
+        ), selected = "Small (Scale)"
+      ),
+      conditionalPanel( 
+        condition = (        
+          "input.BinsHow==0"), 
+              sliderInput(                        
+        inputId = "BinSize",          
+        label = h4("Bin size"),
+        min = 0.1,
+        max = 1,
+        value = 0.3
+        )
+        ),
+      conditionalPanel( 
+        condition = (        
+          "input.BinsHow==1"), 
+        numericInput(                        
+          inputId = "BinSize",          
+          label = h4("Bin size"),
+          min = 1,
+          max = 1000,
+          value = 10
+        )
+      ), 
+      
+      
+      h3("Results Download"),
+      downloadButton("downloadData", "Summary statistics"),                       
+      downloadButton("downloadData2", "Box plots")                            
+    )
+    ,
+    mainPanel(                            
+      tabsetPanel(                         
+        tabPanel(
+          "Input Table",  
+          dataTableOutput(               
+            'inputTable')               
+        ),
+        tabPanel(
+          "Data Chosen",  
+          dataTableOutput(                 
+            'resultTable')                 
+        ),
+        tabPanel(                       
+          "Plot", 
+          plotOutput(                      
+            "resultPlot")            
+        ),
+        tabPanel(
+          "Summary",   
+          verbatimTextOutput(            
+            "resultPrint")                 
+        )
+      )
+    )
+    
+  )
+)
+
+#Część serwerowa aplikacji (zawierąca kod odpowiedzialny za właściwą analizę
+#danych, na bazie danych wejsciowych od użytkownika; wyniki zapisane jako dane
+#wyjściowe)
+server <- function(input,output) {
+  
+  dataReactive <- reactive({               
+    Input <- input$Dataset
+    
+    if (is.null(Input)){
+      return(NULL)
+    } else{
+      DatasetUsed <- as.data.frame(read.csv(Input$datapath))
+      return(DatasetUsed)
+    }
+  })
+  
+  stringReactive <- reactive({            
+    StringsUsed <- c(
+      input$ColumnFactor,
+      input$ColumnData
+    )
+    return(StringsUsed)
+  })
+  
+  dataReactive2 <- reactive({              
+    Input <- input$Dataset
+    
+    if (is.null(Input)){
+      return(NULL)
+    } else{
+      DatasetUsed <- as.data.frame(read.csv(Input$datapath))
+      if (input$ColumnData == "" & input$ColumnFactor == "") {
+        return(NULL)
+      } else { 
+        DatasetUsed <- DatasetUsed[stringReactive()]
+        DatasetUsed[,input$ColumnFactor] <- as.factor(
+          DatasetUsed[,input$ColumnFactor])
+      }
+      return(DatasetUsed)
+    }
+  })
+  
+  vals <- reactiveValues()  
+  
+  output$resultPlot <- renderPlot(        
+    if (is.null(dataReactive2())){          
+      return(NULL)                         
+      
+    } else{
+      PlottedData <- ggplot(                
+        dataReactive2(),                     
+        aes(x=dataReactive2()[,2],          
+            fill = dataReactive2()[,1])      
+      ) +                                    
+        geom_histogram(                         
+          binwidth = input$BinSize,          
+          boundary = 0,                      
+          colour="black"
+        ) + 
+        theme_classic() + 
+        labs(y=NULL,x=NULL) +
+        guides(fill=guide_legend(title=colnames(dataReactive2())[1])) +
+        if (input$Joint == FALSE) {       
+          facet_wrap(~dataReactive2()[,1])
+        } else {                          
+        }
+      vals$ggpl <-PlottedData
+      return(PlottedData)
+    }                                   
+  )                                                                       
+  
+
+Summary <- reactive({
+    if (is.null(dataReactive2())){         
+      return(NULL)                        
+      
+    } else{    
+      if (input$Joint == FALSE) {           
+        for (i in 1:nlevels(dataReactive2()[,1])) {
+          print(
+            levels(dataReactive2()[,1])[i],                
+            row.names = FALSE                
+          )
+          print(
+            summary(
+              subset(
+                dataReactive2(), 
+                dataReactive2()[,1] == levels(dataReactive2()[,1])[i]
+              )[,2]                          
+            ),                               
+            row.names = FALSE
+          )
+        }
+      } else {
+        {summary(dataReactive2())}            
+      }                                    
+    }
+  })
+  
+  
+  output$resultPrint <- renderPrint(       
+    Summary()
+  )                                   
+  
+  output$inputTable <- renderDataTable(  
+    dataReactive(),                        
+    options = list(dom = 'ltp'),          
+    rownames= FALSE                      
+  )
+  
+  output$resultTable <- renderDataTable(   
+    dataReactive2(),                 
+    options = list(dom = 'ltp'),  
+    rownames= FALSE                        
+  )
+  
+  output$downloadData <- downloadHandler(                                      
+    filename = function(){
+      paste("results.txt")
+    },
+    content = function(file) {
+      writeLines(paste(capture.output( {Summary()} )), file)                                                                 
+    }
+  )
+  
+  output$downloadData2 <- downloadHandler(                              
+    filename = function() {"Boxplot.png"},
+    content = function(file) {
+      png(file)
+      print(vals$ggpl)
+      dev.off()
+    }, contentType = 'image/png'
+  )    
+}
+
+#shinyApp() ostatecznie laczy dwie strony aplikacji w funkjonalna calosc
+shinyApp(ui = ui, server = server)
